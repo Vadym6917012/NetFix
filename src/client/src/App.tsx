549558071106fef47835292dc62c3acc5d80987d
Vercel <vercel[bot]@users.noreply.github.com>
2026-03-24 23:01:21 +0000

    Install Vercel Speed Insights
    This is an automated pull request generated by Vercel to help install [Speed Insights](https://vercel.com/docs/speed-insights) in your application.
    
    Co-authored-by: Vercel <vercel[bot]@users.noreply.github.com>

--- src/client/src/App.tsx
+++ src/client/src/App.tsx
@@ -1,6 +1,7 @@
 import React from 'react';
 import 'bootstrap/dist/css/bootstrap.min.css';
 import './styles/globals.css';
+import { SpeedInsights } from '@vercel/speed-insights/react';
 
 import Navbar from './components/Navbar/Navbar';
 import Hero from './components/Hero/Hero';
@@ -20,6 +21,7 @@ const App: React.FC = () => {
         <ContactForm />
       </main>
       <Footer />
+      <SpeedInsights />
     </div>
   );
 };
